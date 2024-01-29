0c478f079f27e2d4d8fa094ef9edea3e96f1a88c
Brandie Lucano <blucano.dev@gmail.com>
2024-01-29 12:32:27 -0800

    working on search shows by term/adding types

--- src/model.ts
+++ src/model.ts
@@ -7,27 +7,35 @@ const TVMAZE_API_URL = "https://api.tvmaze.com/";
  *    Each show object should contain exactly: {id, name, summary, image}
  *    (if no image URL given by API, put in a default image URL)
  */
+interface IShow {
+  id: number;
+  name: string;
+  summary: string;
+  image: string;
+}
 
-async function searchShowsByTerm(term) {
+async function searchShowsByTerm(term: string): Promise<IShow[]> {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-        "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  const searchParams: URLSearchParams = new URLSearchParams({ q: term });
+
+  const resp: Response = await fetch(`${TVMAZE_API_URL}/search/shows?${searchParams}`);
+  const showData = await resp.json();
+
+  console.log("resp and showData:", resp, showData);
+
+  return showData.map((scoreAndShow:IShow)  => {
+
+    return {
+      id: scoreAndShow.id,
+      name: scoreAndShow.name,
+      summary: scoreAndShow.summary,
+      image: scoreAndShow.image
+        ? scoreAndShow.image
+        : MISSING_IMAGE_URL
+    };
+  });
+
+
 }
 
 
